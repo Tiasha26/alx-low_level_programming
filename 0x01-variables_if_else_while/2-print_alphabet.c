d01bd9798745546bc858453c35b455ca2a3c79ec
Tiasha <tednaidu4@gmail.com>
2023-06-16 13:51:20 +0200

    2-print_alphabet.c file added

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main -Starting pont
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alph;
+	for (aplh = 'a'; aplh <= '2'; aplh++)
+	{
+		putchar(aplh);
+	}
+	putchar('\n');
+	return (0);
+}
