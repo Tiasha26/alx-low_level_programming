8f2d88d1a27b99b3175919b6b7ce4026d2e2afd0
Tiasha <tednaidu4@gmail.com>
2023-06-16 17:02:18 +0200

    102-print_comb5.c file added

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,11 +12,11 @@ int main(void)
 	{
 		for (num2 = num1; num2 <= 99; num2++)
 		{
-			putchar('00' + num1 / 10);
-			putchar('00' + num1 % 10);
+			putchar('0' + num1 / 10);
+			putchar('0' + num1 % 10);
 			putchar(' ');
-			putchar('00' + num2 / 10);
-			putchar('00' + num2 % 10);
+			putchar('0' + num2 / 10);
+			putchar('0' + num2 % 10);
 
 		if (num1 != 99 || num2 != 99)
 		{
