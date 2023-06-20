b7fe565b9e499431dc34f72e8657c6038680953e
Tiasha <tednaidu4@gmail.com>
2023-06-20 18:53:36 +0200

    100-times_table.c file modified

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,22 +7,45 @@
 
 void print_times_table(int n)
 {
-	int i, j, product;
+	        int x, y, z, k, t;
 
-	 if (n < 0 || n > 15) 
-	 {
-	         return;
-	 }
-	     for (i = 0; i <= n; i++) {
-		             for (j = 0; j <= n; j++) {
-				                 product = i * j;
-
-						             if (j == 0) {
-								                     printf("%d", product);
-										                 } else {
-													                 printf(", %2d", product);
-															             }
-							             }
-			             printf("\n");
-				         }
+		        for (x = 0; x <= n; x++)
+				        {
+						                for (y = 0; y <= n; y++)
+									                {
+												                        z = x * y;
+															                        if (z == 0 && y == 0)
+																			                                _putchar(z + '0');
+																		                        else if (z > 99)
+																						                        {
+																										                                k = z % 10;
+																														                                t = (z / 10) % 10;
+																																		                                z /= 100;
+																																						                                _putchar(',');
+																																										                                _putchar(' ');
+																																														                                _putchar(z + '0');
+																																																		                                _putchar(t + '0');
+																																																						                                _putchar(k + '0');
+																																																										                        }
+																					                        else if (z > 9)
+																									                        {
+																													                                k = z % 10;
+																																	                                z /= 10;
+																																					                                _putchar(',');
+																																									                                _putchar(' ');
+																																													                                _putchar(' ');
+																																																	                                _putchar(z + '0');
+																																																					                                _putchar(k + '0');
+																																																									                        }
+																								                        else
+																												                        {
+																																                                _putchar(',');
+																																				                                _putchar(' ');
+																																								                                _putchar(' ');
+																																												                                _putchar(' ');
+																																																                                _putchar(z + '0');
+																																																				                        }
+																											                }
+								                _putchar('\n');
+}
 }
