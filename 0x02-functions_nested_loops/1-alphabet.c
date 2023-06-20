a9fa026b0b21e73146baf25b3c64e1a9aeb1e82f
Tiasha <tednaidu4@gmail.com>
2023-06-20 03:45:34 -0700

    1-alphabet.c file added

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *  print-alphabet
+ *  main - Starting point
+ *  Return: 0 if successful
+ */
+void print_alphabet(void)
+{
+	char alph;
+
+	for (alph = 'a'; alph <= 'z'; alph++)
+	{
+		_putchar(alph);
+	}
+
+	_putchar('\n');
+}
