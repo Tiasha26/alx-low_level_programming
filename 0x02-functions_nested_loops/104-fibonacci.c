6277b3bf26c563dd7185faf4838eff3373409302
Tiasha <tednaidu4@gmail.com>
2023-06-20 21:22:14 +0200

    104-fibonacci.c file added

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,20 +7,21 @@
 
 int main(void)
 {
-	int count = 98;
-	unsigned int prev = 1;
-	unsigned int current = 2;
-	unsigned int next;
+	long int a, b, c, d;
 
-	printf("%u, %u", prev, current);
+	b = 1;
+	c = 2;
+	d = 3;
 
-	for (int i = 3; i <= count; i++)
+	for (a = 0; a < 98; a++)
 	{
-		next = prev + current;
-		printf(", %u", next);
-		prev = current;
-		current = next;
+		if (a != 97)
+			printf("%ld, ", b);
+		else
+			printf("%ld\n", b);
+		d = c + d;
+		c = b + c;
+		b = d - c;
 	}
-	printf('\n');
 	return (0);
 }
