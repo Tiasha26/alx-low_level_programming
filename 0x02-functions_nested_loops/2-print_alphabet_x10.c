56888d879487938ef80a31c695cff0d16e0e2768
Tiasha <tednaidu4@gmail.com>
2023-06-20 04:05:45 -0700

    2-print_alphabet_x10.c file added

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints out the alphabet
+ * main - uses built-in function
+ * Return: 0 if successful
+ */
+void print_alphabet_x10(void)
+{
+	char alph;
+	int x;
+	x = 0;
+
+	while (x < 10)
+	{
+		for (alph = 'a'; alph <= 'z'; alph++)
+		_putchar(alph);
+		_putchar('\n');
+		x++;
+	}
+}
