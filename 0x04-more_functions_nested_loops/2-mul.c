59f9bd7ddfc0e0ac598861c92a8f1ec555e14f96
Tiasha <tednaidu4@gmail.com>
2023-06-22 16:37:24 +0200

    5-most_numbers.c file added

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -4,7 +4,7 @@
  * mul - multiply two integers
  * @a:integer one to be multiplied
  * @b:integer two to be multiplied
- * Returns:value of the two multiplied integers
+ * Returns: value of the two multiplied integers
  */
 
 int mul(int a, int b)
--- 0x04-more_functions_nested_loops/5-most_numbers.c
+++ 0x04-more_functions_nested_loops/5-most_numbers.c
@@ -6,16 +6,15 @@
 
 void more_numbers(void)
 {
-	int i;
-	int x;
+	int i, x;
 
-	for (x = 0; x < 10; x++)
+	for (i = 0; i < 10; i++)
 	{
-		for (i = 0; i <= 14; i++)
+		for (x = 0; x <= 14; x++)
 		{
-			if (i > 9)
-				_putchar((i / 10) + '0');
-			_putchar((i % 10) + '0');
+			if (x > 9)
+				_putchar((x / 10) + '0');
+			_putchar((x % 10) + '0');
 		}
 		_putchar('\n');
 	}
