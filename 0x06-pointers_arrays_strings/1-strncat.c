bad1e93e0fce0bed3d51f89b31b5452f44ffb437
Tiasha <tednaidu4@gmail.com>
2023-06-29 16:48:24 +0200

    1-strncat.c file added

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -16,7 +16,7 @@ char *_strncat(char *dest, char *src, int n)
 	j = 0;
 	while (dest[i] != '\0')
 		i++;
-	while(src[i] != '\0' && j < n)
+	while (src[i] != '\0' && j < n)
 	{
 		dest[i] = src[j];
 		i++;
