28a80eb7a2e083f177b35d5e83ea26b2ee91ac0b
Tiasha26 <tednaidu4@gmail.com>
2023-09-28 16:54:21 +0200

    Files updated

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -12,10 +12,6 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	if (index >= sizeof(unsigned long int) * 8)
 		return (-1);
-	unsigned long int mask = 1UL << index;
 
-	if ((n & mask) == 0)
-		return (0);
-	else
-		return (1);
+	return ((int)((n >> index) & 1));
 }
