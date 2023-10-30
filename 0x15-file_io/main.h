e8a8db6bc7387de9e293b1660dbb6ab6a89e6d4b
Tiasha26 <tednaidu4@gmail.com>
2023-10-30 16:12:00 +0200

    All files updated

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -20,7 +20,7 @@ int append_text_to_file(const char *filename, char *text_content);
 void exit_with_error(int code, const char *format, ...);
 void close_file(int fd);
 int open_file(const char *filename, int flags, int mode);
-int copy_file(int from_fd, int to_fd);
+int copy_file(int fd_from, int fd_to);
 int main(int argc, char *argv[]);
 
 void print_elf_header(Elf64_Ehdr *header);
