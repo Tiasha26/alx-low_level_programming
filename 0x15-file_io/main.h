c2dbff3a568bda7b34a47080d9d850e0c5f0454b
Tiasha26 <tednaidu4@gmail.com>
2023-10-30 11:56:02 +0200

    All files updated

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,14 +5,22 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/uio.h>
 #include <elf.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+
+void exit_with_error(int code, const char *format, ...);
+void close_file(int fd);
 int open_file(const char *filename, int flags, int mode);
 int copy_file(int from_fd, int to_fd);
 int main(int argc, char *argv[]);
+
+void print_elf_header(Elf64_Ehdr *ehdr);
 
 #endif /* MAIN_H */
