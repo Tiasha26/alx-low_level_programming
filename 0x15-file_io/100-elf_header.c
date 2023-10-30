fcfbf3e54f858295ce08cdece1ee994630323dab
Tiasha26 <tednaidu4@gmail.com>
2023-10-30 16:05:54 +0200

    All files updated

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,34 +1,48 @@
 #include "main.h"
-
 /**
- * print_elf_header - Display information from the ELF header.
- * @ehdr: Pointer to the ELF header structure.
+ * print_elf_header - Print information from the ELF header.
+ * @header: Pointer to the ELF header.
  */
-void print_elf_header(Elf64_Ehdr *ehdr)
+void print_elf_header(Elf64_Ehdr *header)
 {
-	int i;
-
 	printf("ELF Header:\n");
 	printf("  Magic:   ");
-	for (i = 0; i < EI_NIDENT; i++)
-		printf("%02x%c", ehdr->e_ident[i], i == EI_NIDENT - 1 ? '\n' : ' ');
-	printf("Class: %s\n", ehdr->e_ident[EI_CLASS] == ELFCLASS32 ?
-			"ELF32" : "ELF64");
-	printf("Data: %s\n", ehdr->e_ident[EI_DATA] == ELFDATA2LSB ?
-			"2's complement, little endian" : "2's complement, big endian");
-	printf("Version: %d (current)\n", ehdr->e_ident[EI_VERSION]);
-	printf("OS/ABI: %s\n", ehdr->e_ident[EI_OSABI] == ELFOSABI_SYSV ?
-			"UNIX - System V" :
-			ehdr->e_ident[EI_OSABI] == ELFOSABI_NETBSD ? "UNIX - NetBSD" :
-			ehdr->e_ident[EI_OSABI] == ELFOSABI_SOLARIS ?
-			"UNIX - Solaris" : "<unknown>");
-	printf("ABI Version: %d\n", ehdr->e_ident[EI_ABIVERSION]);
-	printf("Type: %s\n", ehdr->e_type == ET_NONE ? "NONE (Unknown)" :
-			ehdr->e_type == ET_REL ? "REL (Relocatable file)" :
-			ehdr->e_type == ET_EXEC ? "EXEC (Executable file)" :
-			ehdr->e_type == ET_DYN ? "DYN (Shared object file)" :
-			ehdr->e_type == ET_CORE ? "CORE (Core file)" : "<unknown>");
-	printf("Entry point address: 0x%lx\n", (unsigned long)ehdr->e_entry);
+	for (int i = 0; i < EI_NIDENT; i++)
+	{
+		printf("%02x ", header->e_ident[i]);
+	}
+	printf("\n");
+	const char *elf_class = (header->e_ident[EI_CLASS] == ELFCLASS32) ?
+		"ELF32" : "ELF64";
+
+	printf("  Class:            %s\n", elf_class);
+	const char *elf_data = (header->e_ident[EI_DATA] == ELFDATA2LSB) ?
+		"2's complement, little endian" : "2's complement, big endian";
+
+	printf("  Data:                %s\n", elf_data);
+	printf("  Version:  %d (current)\n", (int)header->e_ident[EI_VERSION]);
+	printf("  OS/ABI:                            ");
+	switch (header->e_ident[EI_OSABI])
+	{
+		case ELFOSABI_SYSV:
+			printf("UNIX - System V\n");
+			break;
+		case ELFOSABI_NETBSD:
+			printf("UNIX - NetBSD\n");
+			break;
+		case ELFOSABI_SOLARIS:
+			printf("UNIX - Solaris\n");
+			break;
+		default:
+			printf("<unknown: %d>\n", (int)header->e_ident[EI_OSABI]);
+			break;
+	}
+	printf("  ABI Version:     %d\n", (int)header->e_ident[EI_ABIVERSION]);
+	const char *elf_type = (header->e_type == ET_EXEC) ?
+		"EXEC (Executable file)" : "DYN (Shared object file)";
+
+	printf("  Type:                        %s\n", elf_type);
+	printf("  Entry point address:             0x%lx\n", (long)header->e_entry);
 }
 /**
  * main - Entry point for displaying ELF header information.
@@ -38,29 +52,28 @@ void print_elf_header(Elf64_Ehdr *ehdr)
  */
 int main(int argc, char *argv[])
 {
-	int fd = open(argv[1], O_RDONLY);
-	Elf64_Ehdr ehdr;
-	ssize_t bytes_read = read(fd, &ehdr, sizeof(Elf64_Ehdr));
-
 	if (argc != 2)
 	{
 		dprintf(STDERR_FILENO, "Usage: %s elf_filename\n", argv[0]);
-		return (97);
+		return (98);
 	}
+	int fd = open(argv[1], O_RDONLY);
+
 	if (fd == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't open file %s\n", argv[1]);
 		return (98);
 	}
-	if (bytes_read != sizeof(Elf64_Ehdr) || ehdr.e_ident[EI_MAG0] != ELFMAG0 ||
-			ehdr.e_ident[EI_MAG1] != ELFMAG1 || ehdr.e_ident[EI_MAG2] != ELFMAG2 ||
-			ehdr.e_ident[EI_MAG3] != ELFMAG3)
+	Elf64_Ehdr elf_header;
+
+	if (read(fd, &elf_header, sizeof(Elf64_Ehdr)) != sizeof(Elf64_Ehdr) ||
+			memcmp(elf_header.e_ident, ELFMAG, SELFMAG) != 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Not an ELF file: %s\n", argv[1]);
 		close(fd);
 		return (98);
 	}
-	print_elf_header(&ehdr);
+	print_elf_header(&elf_header);
 	close(fd);
 	return (0);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -10,6 +10,7 @@
 #include <sys/types.h>
 #include <sys/uio.h>
 #include <elf.h>
+#include <string.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
