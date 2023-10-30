18976607fff4911c8564f3de86e8be9f9899d259
Tiasha26 <tednaidu4@gmail.com>
2023-10-30 16:50:58 +0200

    All files updated

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,48 +1,34 @@
 #include "main.h"
+
 /**
- * print_elf_header - Print information from the ELF header.
- * @header: Pointer to the ELF header.
+ * print_elf_header - Display information from the ELF header.
+ * @ehdr: Pointer to the ELF header structure.
  */
-void print_elf_header(Elf64_Ehdr *header)
+void print_elf_header(Elf64_Ehdr *ehdr)
 {
+	int i;
+
 	printf("ELF Header:\n");
 	printf("  Magic:   ");
-	for (int i = 0; i < EI_NIDENT; i++)
-	{
-		printf("%02x ", header->e_ident[i]);
-	}
-	printf("\n");
-	const char *elf_class = (header->e_ident[EI_CLASS] == ELFCLASS32) ?
-		"ELF32" : "ELF64";
-
-	printf("  Class:            %s\n", elf_class);
-	const char *elf_data = (header->e_ident[EI_DATA] == ELFDATA2LSB) ?
-		"2's complement, little endian" : "2's complement, big endian";
-
-	printf("  Data:                %s\n", elf_data);
-	printf("  Version:  %d (current)\n", (int)header->e_ident[EI_VERSION]);
-	printf("  OS/ABI:                            ");
-	switch (header->e_ident[EI_OSABI])
-	{
-		case ELFOSABI_SYSV:
-			printf("UNIX - System V\n");
-			break;
-		case ELFOSABI_NETBSD:
-			printf("UNIX - NetBSD\n");
-			break;
-		case ELFOSABI_SOLARIS:
-			printf("UNIX - Solaris\n");
-			break;
-		default:
-			printf("<unknown: %d>\n", (int)header->e_ident[EI_OSABI]);
-			break;
-	}
-	printf("  ABI Version:     %d\n", (int)header->e_ident[EI_ABIVERSION]);
-	const char *elf_type = (header->e_type == ET_EXEC) ?
-		"EXEC (Executable file)" : "DYN (Shared object file)";
-
-	printf("  Type:                        %s\n", elf_type);
-	printf("  Entry point address:             0x%lx\n", (long)header->e_entry);
+	for (i = 0; i < EI_NIDENT; i++)
+		printf("%02x%c", ehdr->e_ident[i], i == EI_NIDENT - 1 ? '\n' : ' ');
+	printf("Class: %s\n", ehdr->e_ident[EI_CLASS] == ELFCLASS32 ?
+			"ELF32" : "ELF64");
+	printf("Data: %s\n", ehdr->e_ident[EI_DATA] == ELFDATA2LSB ?
+			"2's complement, little endian" : "2's complement, big endian");
+	printf("Version: %d (current)\n", ehdr->e_ident[EI_VERSION]);
+	printf("OS/ABI: %s\n", ehdr->e_ident[EI_OSABI] == ELFOSABI_SYSV ?
+			"UNIX - System V" :
+			ehdr->e_ident[EI_OSABI] == ELFOSABI_NETBSD ? "UNIX - NetBSD" :
+			ehdr->e_ident[EI_OSABI] == ELFOSABI_SOLARIS ?
+			"UNIX - Solaris" : "<unknown>");
+	printf("ABI Version: %d\n", ehdr->e_ident[EI_ABIVERSION]);
+	printf("Type: %s\n", ehdr->e_type == ET_NONE ? "NONE (Unknown)" :
+			ehdr->e_type == ET_REL ? "REL (Relocatable file)" :
+			ehdr->e_type == ET_EXEC ? "EXEC (Executable file)" :
+			ehdr->e_type == ET_DYN ? "DYN (Shared object file)" :
+			ehdr->e_type == ET_CORE ? "CORE (Core file)" : "<unknown>");
+	printf("Entry point address: 0x%lx\n", (unsigned long)ehdr->e_entry);
 }
 /**
  * main - Entry point for displaying ELF header information.
@@ -52,28 +38,29 @@ void print_elf_header(Elf64_Ehdr *header)
  */
 int main(int argc, char *argv[])
 {
+	int fd = open(argv[1], O_RDONLY);
+	Elf64_Ehdr ehdr;
+	ssize_t bytes_read = read(fd, &ehdr, sizeof(Elf64_Ehdr));
+
 	if (argc != 2)
 	{
 		dprintf(STDERR_FILENO, "Usage: %s elf_filename\n", argv[0]);
-		return (98);
+		return (97);
 	}
-	int fd = open(argv[1], O_RDONLY);
-
 	if (fd == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't open file %s\n", argv[1]);
 		return (98);
 	}
-	Elf64_Ehdr elf_header;
-
-	if (read(fd, &elf_header, sizeof(Elf64_Ehdr)) != sizeof(Elf64_Ehdr) ||
-			memcmp(elf_header.e_ident, ELFMAG, SELFMAG) != 0)
+	if (bytes_read != sizeof(Elf64_Ehdr) || ehdr.e_ident[EI_MAG0] != ELFMAG0 ||
+			ehdr.e_ident[EI_MAG1] != ELFMAG1 || ehdr.e_ident[EI_MAG2] != ELFMAG2 ||
+			ehdr.e_ident[EI_MAG3] != ELFMAG3)
 	{
 		dprintf(STDERR_FILENO, "Error: Not an ELF file: %s\n", argv[1]);
 		close(fd);
 		return (98);
 	}
-	print_elf_header(&elf_header);
+	print_elf_header(&ehdr);
 	close(fd);
 	return (0);
 }
--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,26 +1,45 @@
 #include "main.h"
+int open_file(const char *filename, int flags, int mode);
+int copy_file(int from_fd, int to_fd);
 /**
- * exit_with_error - Print an error message and exit with the specified code.
- * @code: The exit code.
- * @format: The format of the error message.
- * Return: void.
+ * main - Copies the content of a file to another file.
+ * @argc: The number of command-line arguments.
+ * @argv: An array of command-line argument strings.
+ *
+ * Return: 0 on success, or the corresponding error code on failure.
  */
-void exit_with_error(int code, const char *format, ...)
+int main(int argc, char *argv[])
 {
-	va_list args;
+	int fd_from, fd_to;
 
-	va_start(args, format);
-	dprintf(STDERR_FILENO, format, args);
-	va_end(args);
-	exit(code);
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
+		exit(97);
+	}
+	fd_from = open_file(argv[1], O_RDONLY, 0);
+	fd_to = open_file(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
+	if (copy_file(fd_from, fd_to) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		close(fd_from);
+		close(fd_to);
+		exit(99);
+	}
+	close(fd_from);
+	close(fd_to);
+	return (0);
 }
 
 /**
- * open_file - Open a file with specified flags and mode, and handle errors.
- * @filename: The name of the file to open.
- * @flags: The flags to use when opening the file.
- * @mode: The mode to set for the file.
- * Return: The file descriptor if successful.
+ * open_file - Opens a file with specified flags and mode.
+ * @filename: The name of the file.
+ * @flags: File opening flags.
+ * @mode: File permissions mode.
+ *
+ * Return: File descriptor on success, or exits with an error
+ * message on failure.
  */
 int open_file(const char *filename, int flags, int mode)
 {
@@ -28,72 +47,34 @@ int open_file(const char *filename, int flags, int mode)
 
 	if (fd == -1)
 	{
-		exit_with_error(99, "Error: Can't open file %s\n", filename);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
+		exit(98);
 	}
 	return (fd);
 }
 
 /**
- * close_file - Close a file descriptor and handle errors.
- * @fd: The file descriptor to close.
- * Return: void.
+ * copy_file - Copies data from one file descriptor to another.
+ * @from_fd: Source file descriptor.
+ * @to_fd: Destination file descriptor.
+ *
+ * Return: 0 on success, -1 on failure.
  */
-void close_file(int fd)
-{
-	if (close(fd) == -1)
-	{
-		exit_with_error(100, "Error: Can't close fd %d\n", fd);
-	}
-}
-
-/**
- * copy_file - Copy the content from one file descriptor to another.
- * @fd_from: The source file descriptor.
- * @fd_to: The destination file descriptor.
- * Return: void.
- */
-void copy_file(int fd_from, int fd_to)
+int copy_file(int from_fd, int to_fd)
 {
 	char buffer[1024];
-	ssize_t bytes_read, bytes_written;
+	ssize_t read_bytes, write_bytes;
 
-	while ((bytes_read = read(fd_from, buffer, sizeof(buffer)) > 0))
+	while ((read_bytes = read(from_fd, buffer, 1024)) > 0)
 	{
-		bytes_written = write(fd_to, buffer, bytes_read);
-		if (bytes_written == -1)
-		{
-			exit_with_error(99, "Error: Can't write to file\n");
-		}
-	}
+		write_bytes = write(to_fd, buffer, read_bytes);
 
-	if (bytes_read == -1)
+		if (write_bytes == -1 || write_bytes != read_bytes)
+			return (-1);
+	}
+	if (read_bytes == -1)
 	{
-		exit_with_error(98, "Error: Can't read from file\n");
+		return (-1);
 	}
-}
-
-/**
- * main - The main function for copying a file.
- * @argc: The number of command-line arguments.
- * @argv: An array of command-line argument strings.
- * Return: 0 on success, other values on failure.
- */
-int main(int argc, char *argv[])
-{
-	char *file_from = argv[1];
-	char *file_to = argv[2];
-
-	if (argc != 3)
-	{
-		exit_with_error(97, "Usage: cp file_from file_to\n");
-	}
-	int fd_from = open_file(file_from, O_RDONLY, 0);
-	int fd_to = open_file(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
-
-	copy_file(fd_from, fd_to);
-
-	close_file(fd_from);
-	close_file(fd_to);
-
 	return (0);
 }
