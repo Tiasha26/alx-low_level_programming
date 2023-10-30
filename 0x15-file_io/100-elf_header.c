9f540f3e53e9232b451ee2ce9861e5b7e60af229
Tiasha26 <tednaidu4@gmail.com>
2023-10-30 12:04:17 +0200

    All files updated

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -17,7 +17,8 @@ void print_elf_header(Elf64_Ehdr *ehdr)
 	printf("Data: %s\n", ehdr->e_ident[EI_DATA] == ELFDATA2LSB ?
 			"2's complement, little endian" : "2's complement, big endian");
 	printf("Version: %d (current)\n", ehdr->e_ident[EI_VERSION]);
-	printf("OS/ABI: %s\n", ehdr->e_ident[EI_OSABI] == ELFOSABI_SYSV ? "UNIX - System V" :
+	printf("OS/ABI: %s\n", ehdr->e_ident[EI_OSABI] == ELFOSABI_SYSV ?
+			"UNIX - System V" :
 			ehdr->e_ident[EI_OSABI] == ELFOSABI_NETBSD ? "UNIX - NetBSD" :
 			ehdr->e_ident[EI_OSABI] == ELFOSABI_SOLARIS ?
 			"UNIX - Solaris" : "<unknown>");
@@ -29,6 +30,12 @@ void print_elf_header(Elf64_Ehdr *ehdr)
 			ehdr->e_type == ET_CORE ? "CORE (Core file)" : "<unknown>");
 	printf("Entry point address: 0x%lx\n", (unsigned long)ehdr->e_entry);
 }
+/**
+ * main - Entry point for displaying ELF header information.
+ * @argc: The number of command-line arguments.
+ * @argv: An array of command-line argument strings.
+ * Return: 0 on success, other values on failure.
+ */
 int main(int argc, char *argv[])
 {
 	int fd = open(argv[1], O_RDONLY);
